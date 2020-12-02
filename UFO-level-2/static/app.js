de114c31cd070781f9cf4d00bf40919c4bfe6f28
sapiesitupou <sapiesit@gmail.com>
2020-12-02 02:19:29 -0800

    level 2.1

--- UFO-level-2/static/app.js
+++ UFO-level-2/static/app.js
@@ -4,3 +4,21 @@
 var tableData = data;
 
 
+// Viewing the available data fromt he data.js
+// console.log(tableData);
+
+
+// Creating References
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+
+// @TODO : Create said buttons for new said var inputs
+// var inputFieldState = d3.select("#state");
+// var inputFieldCountry = d3.select("#country");
+// var inputFieldShape = d3.select("shape");
+
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// console.log(columns);
+

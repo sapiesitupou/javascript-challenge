6bcc9c0099bf1dbc9797f04e1749d5a78ce3d2aa
sapiesitupou <sapiesit@gmail.com>
2020-12-02 02:34:56 -0800

    filters added

--- UFO-level-2/static/app.js
+++ UFO-level-2/static/app.js
@@ -22,3 +22,35 @@ var inputFieldCity = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 // console.log(columns);
 
+// Inputting the data into HTML
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+            )
+    });
+}
+
+addData(tableData);
+
+//Create an Event Listener for the button
+//Set up Filter Button for date and city
+button.on("click", ()=> {
+
+    d3.event.preventDefault();
+
+    var inputDate = inputFieldDate.property("value").trim();
+    // console.log(inputDate)
+    var inputCity = inputFieldCity.property("value").toLowerCase().trim();
+
+
+    // console.log(inputCity)
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    // console.log(filterDate)
+    var filterCity = tableData.filter(tableData => tableData.city === inputCity);
+    //console.log(filterCity)
+
+    var filterCombinedData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity);
+    //console.log(filterCombinedData)
+
+})
